0f31dcb005cb565690cdf0a8acfbcccdd10fd7f4
jkelly <joey@joeykelly.net>
2017-11-28 21:56:03 -0500

    environment stripping, comments and formatting cleanup

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# slackware-service
+slackware-service
 A simple wrapper around Slackware RC scripts
 
 SETUP:
-You'll probably want to alias this in /root/.bashrc
-  alias service='/root/bin/slackwareservice.pl'
+  You'll probably want to alias this in /root/.bashrc
+    alias service='/root/bin/slackwareservice.pl'
 
 USAGE:
-  root@daniel:~# ps auxw | grep named
+  root@cowabongos:~# ps auxw | grep named
   root      3991  0.0  0.0   2748   776 pts/22   S+   20:18   0:00 grep named
-  root@daniel:~# service bind start
-  Smartmatch is experimental at /root/bin/service.pl line 26.
+  root@cowabongos:~# service bind start
   Starting BIND:  /usr/sbin/named
-  root@daniel:~# ps auxw | grep named
+  root@cowabongos:~# ps auxw | grep named
   root      3995  1.6  0.5  54380 14540 ?        Ssl  20:18   0:00 /usr/sbin/named
   root      4004  0.0  0.0   2748   776 pts/22   S+   20:18   0:00 grep named
--- service.pl
+++ service.pl
@@ -11,20 +11,27 @@
 
 use strict;
 #use warnings;
+# we don't want any "experimental" warnings, so...
+no warnings;
 
 use 5.18.1;
 
 my $rc = '/etc/rc.d/rc.';
 my @actions = qw(start stop restart status);
 
+# what service do we want to manage?
 my $service = shift;
 $service = safechars($service);
+
 my $daemon = $rc . $service;
 if (-x $daemon) {
+  # we have a valid service, so what do we want to do with it?
   my $action = shift;
   $action = safechars($action);
   if ($action ~~ @actions) {
-    system "$daemon $action";
+    # we have a valid service and a plausible action, so let's do it
+    # ...but we want to vacate any shell environment variables, etc., so we use "env -i"
+    system "env -i $daemon $action";
   } else {
     say "$action is not a valid action";
   }
@@ -33,6 +40,7 @@ if (-x $daemon) {
 }
 
 
+# let's sanitize user input, like a good programmer should...
 sub safechars {
   my $string = shift;
   $string =~ tr/a-zA-Z0-9//dc;
